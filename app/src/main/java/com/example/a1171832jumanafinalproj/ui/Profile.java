f2d368ef0cb06ed7effa714a4ad275341698ded3
ayoub mazrou <ga_mazrou@esi.dz>
2021-05-29 12:10:46 +0100

    change images

--- app/src/main/java/com/example/a1171832jumanafinalproj/ui/Profile.java
+++ app/src/main/java/com/example/a1171832jumanafinalproj/ui/Profile.java
@@ -1,16 +1,28 @@
 package com.example.a1171832jumanafinalproj.ui;
 
+import static android.app.Activity.RESULT_OK;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,8 +36,11 @@ import android.widget.Toast;
 import com.example.a1171832jumanafinalproj.DataBaseHelper;
 import com.example.a1171832jumanafinalproj.R;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Profile extends Fragment {
@@ -33,7 +48,7 @@ public class Profile extends Fragment {
     private ProfileViewModel ProfileViewModel;
     String email;
     private DataBaseHelper dataBaseHelper; //here
-
+    private ImageView avatar ;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -50,28 +65,88 @@ public class Profile extends Fragment {
     }
 
 
+
+    private void pickFromGallery(int requestCode) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, requestCode);
+    }
+
+
+    private void dispatchTakePictureIntent(int requestCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+     //
+        startActivityForResult(takePictureIntent, requestCode);
+
+    }
+
+    final int GALLERY_REQUEST_CODE = 100 ;
+    final int REQUEST_IMAGE_CAPTURE = 101 ;
+    final int PICK_IMAGE_REQUEST_CODE = 102 ;
+    Bitmap bitmap ;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            pickFromGallery(GALLERY_REQUEST_CODE);
+        }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent(PICK_IMAGE_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+            super.onActivityResult(reqCode, resultCode, data);
+
+            if (resultCode == RESULT_OK && (data.getData() != null||data.getExtras()!=null)) {
+                if (reqCode == GALLERY_REQUEST_CODE) {
+                    try {
+                       final Uri imageUri = data.getData();
+                        final InputStream imageStream = Objects.requireNonNull(getActivity()).getContentResolver().openInputStream(imageUri);
+                        bitmap = BitmapFactory.decodeStream(imageStream);
+                        avatar.setImageBitmap(bitmap);
+
+                    } catch (FileNotFoundException e) {
+                         e.printStackTrace();
+                    }
+                } else if (reqCode == PICK_IMAGE_REQUEST_CODE && data.getExtras() != null) {
+                     Bundle extras = data.getExtras();
+                     bitmap = (Bitmap) extras.get("data");
+                    avatar.setImageBitmap(bitmap);
+                }
+            }
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        final TextView nameonprofile = getActivity().findViewById(R.id.editprofile_name_phone);
+        final TextView detailpass = getActivity().findViewById(R.id.editprofile_detailpass);
+        avatar = requireActivity().findViewById(R.id.image_view) ;
+        final ImageView editbtn = getActivity().findViewById(R.id.editprofile_editbtn);
+        final Button savebtn = getActivity().findViewById(R.id.editprofile_savebtn);
+
+        final EditText firstnamefield = (EditText) getActivity().findViewById(R.id.editProfileFirstnameBox);
+        final EditText lastnamefield = (EditText) getActivity().findViewById(R.id.editProfileLastnameBox);
+        final EditText phonefield = (EditText) getActivity().findViewById(R.id.editProfilePhoneBox);
+        final EditText passfield = (EditText) getActivity().findViewById(R.id.editProfilePassBox);
+        final EditText confirmpassfield = (EditText) getActivity().findViewById(R.id.editProfilePassBox2);
         if ( getArguments() != null && !getArguments().isEmpty()) {
             ProfileArgs args = ProfileArgs.fromBundle(getArguments());
 
             email = args.getEmail();
-            Log.e("Errorrrr " , email) ;
+
             final String[] editeddata = new String[4];
 
 
-            final TextView nameonprofile = getActivity().findViewById(R.id.editprofile_name_phone);
-            final TextView detailpass = getActivity().findViewById(R.id.editprofile_detailpass);
 
-            final ImageView editbtn = getActivity().findViewById(R.id.editprofile_editbtn);
-            final Button savebtn = getActivity().findViewById(R.id.editprofile_savebtn);
 
-            final EditText firstnamefield = (EditText) getActivity().findViewById(R.id.editProfileFirstnameBox);
-            final EditText lastnamefield = (EditText) getActivity().findViewById(R.id.editProfileLastnameBox);
-            final EditText phonefield = (EditText) getActivity().findViewById(R.id.editProfilePhoneBox);
-            final EditText passfield = (EditText) getActivity().findViewById(R.id.editProfilePassBox);
-            final EditText confirmpassfield = (EditText) getActivity().findViewById(R.id.editProfilePassBox2);
+
 
 
             dataBaseHelper = new DataBaseHelper(getActivity(), "database", null, 1);
@@ -202,8 +277,35 @@ public class Profile extends Fragment {
             });
 
         }
-    }
+        editbtn.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v) {
+                addImage();
+            }
+        });
+    }
+    private void addImage() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getContext()));
+        String[] choices = {getText(R.string.import_image).toString(), getText(R.string.take_photo).toString()};
+        builder.setSingleChoiceItems(choices, -1, ((dialog, which) -> {
+            if (which == 0) {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                    ActivityCompat.requestPermissions(Objects.requireNonNull(getActivity()), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+                else
+                    pickFromGallery(GALLERY_REQUEST_CODE);
+            } else {
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
+                    ActivityCompat.requestPermissions(Objects.requireNonNull(getActivity()), new String[]{Manifest.permission.CAMERA}, REQUEST_IMAGE_CAPTURE);
+                else
+                    dispatchTakePictureIntent(PICK_IMAGE_REQUEST_CODE);
+            }
+            dialog.dismiss();
+        }));
+        builder.setTitle(getText(R.string.select));
+        builder.setNegativeButton(getText(R.string.cancel), (dialog, which) -> dialog.dismiss());
+        builder.create().show();
+    }
 
 
     public static boolean isValid(String passwordhere, String confirmhere, List<String> errorList) {
